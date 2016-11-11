91af3620e0bb766d36282d3ce2f153d4a6be8df7
jk1510karti <karthicksanthosh@live.com>
2016-11-11 15:16:25 +0530

    sgsd

--- README.txt
+++ README.txt
@@ -1,5 +1,3 @@
-A Pen created at CodePen.io. You can find this one at http://codepen.io/suez/pen/dPqxoM.
+A Pen created at CodePen.io. You can find this one at http://codepen.io/JonasBadalic/pen/dPyGWw.
 
- Based on this - https://dribbble.com/shots/1945593-Login-Home-Screen
-
-Right now svg-icons not appears in IE (css animation). All 4 icons are hardcoded with numbers, just for fun :D
+ A login form UI with some simple css transitions.
--- js/index.js
+++ js/index.js
@@ -1,62 +1,74 @@
-$(document).ready(function() {
+
+var form = $('.form');
+var btn = $('#submit');
+var topbar = $('.topbar');
+var input = $('#password');
+var article =$('.article');
+var tries = 0;
+var h = input.height();
+$('.spanColor').height(h+23);
+$('#findpass').on('click',function(){
+  $(this).text('this-is-soo-cool');
+});
+input.on('focus',function(){
+  topbar.removeClass('error success');
+  input.text('');
+});
+btn.on('click',function(){
+  if(tries<=2){
+    var pass = $('#password').val();
+    console.log(pass);
+    if(pass==='this-is-soo-cool'){
+    setTimeout(function(){
+      btn.text('Success!');
+    },250);
+    topbar.addClass('success');
+    form.addClass('goAway');
+    article.addClass('active');
+    tries=0;
+  }
+    else{
+      topbar.addClass('error');
+      tries++;
+      switch(tries){
+        case 0:
+          btn.text('Login');
+          break;
+        case 1:
+          setTimeout(function(){
+          btn.text('You have 2 tries left');
+          },300);
+          break;
+        case 2:
+          setTimeout(function(){
+          btn.text('Only 1 more');
+          },300);
+          break;
+        case 3:
+          setTimeout(function(){
+          btn.text('Recover password?');
+          },300);
+          input.prop('disabled',true);
+          topbar.removeClass('error');
+          input.addClass('disabled');
+          btn.addClass('recover');
+          break;
+         defaut:
+          btn.text('Login');
+          break;
+      }
+    } 
+  }
+  else{
+    topbar.addClass('disabled');
+  }
   
-  var animating = false,
-      submitPhase1 = 1100,
-      submitPhase2 = 400,
-      logoutPhase1 = 800,
-      $login = $(".login"),
-      $app = $(".app");
-  
-  function ripple(elem, e) {
-    $(".ripple").remove();
-    var elTop = elem.offset().top,
-        elLeft = elem.offset().left,
-        x = e.pageX - elLeft,
-        y = e.pageY - elTop;
-    var $ripple = $("<div class='ripple'></div>");
-    $ripple.css({top: y, left: x});
-    elem.append($ripple);
-  };
-  
-  $(document).on("click", ".login__submit", function(e) {
-    if (animating) return;
-    animating = true;
-    var that = this;
-    ripple($(that), e);
-    $(that).addClass("processing");
-    setTimeout(function() {
-      $(that).addClass("success");
-      setTimeout(function() {
-        $app.show();
-        $app.css("top");
-        $app.addClass("active");
-      }, submitPhase2 - 70);
-      setTimeout(function() {
-        $login.hide();
-        $login.addClass("inactive");
-        animating = false;
-        $(that).removeClass("success processing");
-      }, submitPhase2);
-    }, submitPhase1);
-  });
-  
-  $(document).on("click", ".app__logout", function(e) {
-    if (animating) return;
-    $(".ripple").remove();
-    animating = true;
-    var that = this;
-    $(that).addClass("clicked");
-    setTimeout(function() {
-      $app.removeClass("active");
-      $login.show();
-      $login.css("top");
-      $login.removeClass("inactive");
-    }, logoutPhase1 - 120);
-    setTimeout(function() {
-      $app.hide();
-      animating = false;
-      $(that).removeClass("clicked");
-    }, logoutPhase1);
-  });
-  
+});
+
+$('.form').keypress(function(e){
+   if(e.keyCode==13)
+   submit.click();
+});
+input.keypress(function(){
+  topbar.removeClass('success error');
 });
